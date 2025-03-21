f113ddfa6168c864ee7a3639a8f7eb744a48944a
skSadi12 <sksadi433@gmail.com>
2025-03-21 22:08:42 +0600

    herosection component added

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+export default function HeroSection() {
+    return (
+      <div className="bg-darkBlue text-white text-center p-12">
+        <h1 className="text-4xl font-bold">
+          More <span className="italic">leads</span>, less{" "}
+          <span className="italic">people</span>.
+        </h1>
+        <p className="mt-4">Train an aiDR on your ICP and messaging matrix.</p>
+        <button className="mt-6 px-6 py-2 bg-primary rounded-lg">
+          Sign Up for the Beta
+        </button>
+      </div>
+    );
+  }
+  
